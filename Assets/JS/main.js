6afdc0647649ddbed07fc65b7e629ca93e350479
fire8327 <fire83274@gmail.com>
2026-07-12 17:35:31 +0300

    feat: create switchButtons

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,3 +2,13 @@
 $("#toggler").click(() => {
     $("#menu").toggleClass("max-lg:top-0 max-lg:top-full max-lg:translate-y-[-600px]")
 })
+
+/* buttons */
+$(".switchButton").each((i, el) => {
+    $(el).click(() => {
+        $(".switchButton").addClass("bg-white")
+        $(".switchButton").removeClass("text-white")
+        $(el).addClass("text-white bg-[#1A1A1A]")
+        $(el).removeClass("bg-white")
+    })
+})
