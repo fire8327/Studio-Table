33b8a6060ecbeca97727c5efc447b0afdcfc6641
fire8327 <fire83274@gmail.com>
2026-07-12 22:39:21 +0300

    feat: switcher

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -3,15 +3,20 @@ $("#toggler").click(() => {
     $("#menu").toggleClass("max-lg:top-0 max-lg:top-full max-lg:translate-y-[-600px]")
 })
 
-/* buttons */
-$(".switchButton").each((i, el) => {
-    $(el).click(() => {
-        $(".switchButton").addClass("bg-white")
-        $(".switchButton").removeClass("text-white")
-        $(el).addClass("text-white bg-[#1A1A1A]")
-        $(el).removeClass("bg-white")
-    })
-})
-
 /* tabs */
 $("#tabs").tabs()
+
+let totalTabs = $("#tabs").find(".ui-tabs-panel").length // общее количество вкладок
+
+$("#tabNextButton").click(function() {
+    const activeIndex = $("#tabs").tabs("option", "active")
+    if(activeIndex < 7) {
+        $("#tabs").tabs("option", "active", (activeIndex + 1) % totalTabs)
+    }
+})
+$("#tabPrevButton").click(function() {
+    const activeIndex = $("#tabs").tabs("option", "active")
+    if(activeIndex !== 0) {
+        $("#tabs").tabs("option", "active", (activeIndex - 1 + totalTabs) % totalTabs)
+    }
+})
